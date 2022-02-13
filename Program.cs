44b5392498baa3cfbaf35c50d99f59f69e68e2d2
mabbuprem <mabbupremkumar@gmail.com>
2022-02-13 20:44:21 +0530

    position after every die roll

--- Program.cs
+++ Program.cs
@@ -18,43 +18,54 @@ namespace SnakeLadderProblems
             const int SNAKE = 2;
             const int NO_PLAY = 3;
 
+            int rollyTry = 0;
+
             DiceRolling diceRolling = new DiceRolling();
             UserInfo user = new UserInfo();
 
 
-            //variables
-            int diceNumber = diceRolling.diceRoll();
-            int functionNumber = diceRolling.Function();
-            //Console.WriteLine(diceNumber)
             while (user.UserPosition < 100)
             {
 
+                //variables
+                int diceNumber = diceRolling.diceRoll();
+                int functionNumber = diceRolling.Function();
+                //Console.WriteLine(diceNumber)
+
                 switch (functionNumber)
-            {
-                case LADDER:
-                    user.UserPosition += diceNumber;
-                    break;
-                case SNAKE:
-                  if  (user.UserPosition += diceNumber);
-                   {
-                       user.UserPosition -= diceNumber;
-                   }
-                   else
-                   {
-                        user.UserPosition = 0;
-                        Console.WriteLine("user position nagative");
+                {
+                    case LADDER:
+                        //user .UserPosition= user .UserPosition + dicenumber <= 100? user .Userposition: user .userPosition + diceNumber; 
+                        if (user.UserPosition += diceNumber) ;
+                        {
+                            user.UserPosition -= diceNumber;
+                        }
+                        break;
+                    case SNAKE:
+                        if (user.UserPosition += diceNumber) ;
+                        {
+                            user.UserPosition -= diceNumber;
+                        }
+                        else
+                        {
+                            user.UserPosition = 0;
+                            Console.WriteLine("user position nagative");
 
                         }
-                    break;
-                case NO_PLAY:
-                    Console.WriteLine("on play");
-                    break;
-                default:
-                    Console.WriteLine("defult case error");
-                    break;
+                        break;
+                    case NO_PLAY:
+                        Console.WriteLine("on play");
+                        break;
+                    default:
+                        Console.WriteLine("defult case error");
+                        break;
+
+                }
+                rollyTry++;
+                Console.WriteLine($"position; {user.UserPosition}");
+                Console.WriteLine($"on. of try: {rollyTry}");
 
             }
-            Console.WriteLine(user.UserPosition);
         }
 
     }
