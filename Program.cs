32f3ad1b393e0e417760898414053ad68d52d489
mabbuprem <mabbupremkumar@gmail.com>
2022-02-13 20:04:36 +0530

    UserPosition100

--- Program.cs
+++ Program.cs
@@ -18,20 +18,33 @@ namespace SnakeLadderProblems
             const int SNAKE = 2;
             const int NO_PLAY = 3;
 
-            int position = 0;
             DiceRolling diceRolling = new DiceRolling();
             UserInfo user = new UserInfo();
-            int diceNumber = diceRolling.DiceRoll();
-            int functionNumber = diceRolling.function();
-            Console.WriteLine(diceNumber);
 
-            switch (functionNumber)
+
+            //variables
+            int diceNumber = diceRolling.diceRoll();
+            int functionNumber = diceRolling.Function();
+            //Console.WriteLine(diceNumber)
+            while (user.UserPosition < 100)
+            {
+
+                switch (functionNumber)
             {
                 case LADDER:
                     user.UserPosition += diceNumber;
                     break;
                 case SNAKE:
-                    user.UserPosition += diceNumber;
+                  if  (user.UserPosition += diceNumber);
+                   {
+                       user.UserPosition -= diceNumber;
+                   }
+                   else
+                   {
+                        user.UserPosition = 0;
+                        Console.WriteLine("user position nagative");
+
+                        }
                     break;
                 case NO_PLAY:
                     Console.WriteLine("on play");
